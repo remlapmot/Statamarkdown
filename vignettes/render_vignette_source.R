a137629eac690e3590a4fd848be7f8de2223aca6
Tom Palmer <remlapmot@hotmail.com>
2026-07-16 06:52:44 +0100

    Use ropensci precompute vignettes trick
    As per https://ropensci.org/blog/2019/12/08/precompute-vignettes/

--- vignettes/render_vignette_source.R
+++ vignettes/render_vignette_source.R
@@ -1,8 +1,3 @@
-library(rmarkdown)
-
-render("vignettes/basicuse.rmd", output_format=html_vignette(),
-       output_file="1_Basic_Use_of_Statamarkdown.html", output_dir="inst/doc")
-render("vignettes/linkblocks.Rmd", output_format=html_vignette(),
-       output_file="2_Linking_Stata_Code_Chunks.html", output_dir="inst/doc")
-render("vignettes/randstata.rmd", output_format=html_vignette(),
-       output_file="3_Combining_Stata_and_R.html", output_dir="inst/doc")
+knitr::knit("vignettes/basicuse.Rmd.orig", output = "vignettes/basicuse.Rmd")
+knitr::knit("vignettes/linkblocks.Rmd.orig", output = "vignettes/linkblocks.Rmd")
+knitr::knit("vignettes/randstata.Rmd.orig", output = "vignettes/randstata.Rmd")
