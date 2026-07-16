a3a3fff2bf76b2b0c58005cbfcb47852f39f7951
Tom Palmer <remlapmot@hotmail.com>
2026-07-16 07:18:24 +0100

    Update render_vignette_source.R

--- vignettes/render_vignette_source.R
+++ vignettes/render_vignette_source.R
@@ -1,3 +1,21 @@
-knitr::knit("vignettes/basicuse.Rmd.orig", output = "vignettes/basicuse.Rmd")
-knitr::knit("vignettes/linkblocks.Rmd.orig", output = "vignettes/linkblocks.Rmd")
-knitr::knit("vignettes/randstata.Rmd.orig", output = "vignettes/randstata.Rmd")
+# Precompute the vignettes, which require Stata, following
+# https://ropensci.org/blog/2019/12/08/precompute-vignettes/
+# Run from the package root directory:
+#   source("vignettes/render_vignette_source.R")
+#
+# Each vignette is knit in a fresh R process (as R CMD build does)
+# because Statamarkdown sets the engine.path chunk option in .onAttach,
+# and knitr::knit() restores chunk options when it finishes -- so a
+# second knit in the same session would find engine.path unset.
+#
+# The knits are run from within vignettes/ so that the chunks, the
+# knitr hooks, and Stata itself all share the same working directory
+# (the collectcode option in linkblocks relies on this).
+xfun::in_dir("vignettes", {
+  for (v in c("basicuse", "linkblocks", "randstata")) {
+    xfun::Rscript_call(
+      knitr::knit,
+      list(input = paste0(v, ".Rmd.orig"), output = paste0(v, ".Rmd"), quiet = TRUE)
+    )
+  }
+})
